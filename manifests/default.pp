a2c07190543b3af29aab3c06763a3f25598d31dd
Przybylski 중광 <r351574nc3@gmail.com>
2012-10-10 01:36:36 -0700

    Trying to figure out which is the correct paramter

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,10 +1,11 @@
 class kuali {
 	user { "kuali":
-		groups          => 'kuali',
-		comment         => 'This user was created by Puppet',
-		ensure          => present,
-		provider        => 'useradd',
-		manages_homedir => true
+		groups     => 'kuali',
+		password   => 'kuali',
+		comment    => 'This user was created by Puppet',
+		ensure     => present,
+		provider   => 'useradd',
+		managehome => true
 	}
 }
 
