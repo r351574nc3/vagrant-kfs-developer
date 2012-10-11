804c57b8df04ddba5fdc7f5f289df8e10b82bd56
Przybylski 중광 <r351574nc3@gmail.com>
2012-10-10 22:12:40 -0700

    Moving groups to kuali puppet

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,12 +1,17 @@
 class kuali {
+    group { "eclipse":
+        ensure => present,
+    }
+
 	user { "kuali":
 		groups     => 'kuali',
-		password   => 'kuali',
+		password   => ['kuali', 'wheel', 'admin', 'eclipse'],
 		comment    => 'This user was created by Puppet',
 		ensure     => present,
 		provider   => 'useradd',
 		managehome => true
 	}
+
 }
 
 include kuali
